5b267534d9be55dc863fc81057f2b32d25ef5f17
Daniel Holguin <d.holguin@protonmail.com>
2025-06-12 02:10:26 -0700

    Update (#1)
    * add style sheet
    
    * Add some styling

---
+++ src/main.js
@@ -0,0 +1,20 @@
+const reticle = document.getElementById("reticle");
+
+let mouseX = 0;
+let mouseY = 0;
+let reticleX = 0;
+let reticleY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function animate() {
+  reticleX += (mouseX - reticleX) * 0.04;
+  reticleY += (mouseY - reticleY) * 0.04;
+  reticle.style.transform = `translate(${reticleX - 12.5}px, ${reticleY - 12.5}px)`;
+  requestAnimationFrame(animate);
+}
+
+animate();
